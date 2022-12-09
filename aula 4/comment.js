dc6360273d6e181acd9086fc8ad5da4e82d7dc7a
Marcos Gabriel <55326111+MarcosGBPR@users.noreply.github.com>
2022-12-08 21:05:49 -0300

    a

---
+++ aula 4/comment.js
@@ -0,0 +1,11 @@
+// Testando comentário
+console.log ('Hello World!'); // Outro Comentário
+console.log ('Linha 1');
+console.log ('Linha 2');
+// Comentário
+console.log ('Linha 3');
+/* Teste de comentário
+Entre linhas */
+console.log ('Linha 4');
+console.log ('Linha 5');
+// Vai ser ignorado
