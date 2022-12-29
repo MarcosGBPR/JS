a22bb7377de2fb7667ea7c77610f52227e3dec56
Marcos Gabriel <55326111+MarcosGBPR@users.noreply.github.com>
2022-12-28 23:38:14 -0300

    att

---
+++ aula 6/index.js
@@ -0,0 +1,14 @@
+let nome; // Declarou a variável
+nome = 'João'; // Inicializando a variável
+console.log(nome);
+nome = 'Otávio'; // Alteração da variável
+console.log(nome);
+
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com 'let'
+// Não utilize VAR, utilize LET
