0d59f9fdb821a1a6cbcf6f3aded92d33ccec96c1
Marcos Gabriel <55326111+MarcosGBPR@users.noreply.github.com>
2023-01-02 17:02:13 -0300

    a

---
+++ aula 8/index.js
@@ -0,0 +1,15 @@
+/*
+Luiz Otávio Miranda tem 30 anos, pesa 84KG, tem 1,80 de altura e seu IMC é de 25.925925925925924
+Luiz Otávio nasceu em 1993
+*/
+
+const nome = 'Luiz Otávio';
+const sobrenome = 'Miranda';
+const idade = 30;
+const peso = 84;
+const altura = 1.80;
+let imc; // peso / (altura* altura)
+let anoNascimento;
+
+var imc = peso / (altura * altura);
+var imc = 1993 - idade;
