e19e5e9f70d46cf5156988f5bdea11cc2414e62e
Marcos Gabriel <55326111+MarcosGBPR@users.noreply.github.com>
2023-01-30 17:08:57 -0300

    a

---
+++ aula 16/index.js
@@ -0,0 +1,24 @@
+//               1       2        3          4      5
+const alunos = ['Luiz', 'Maria', 'João']; // Luiza, Eduardo
+
+alunos.push('Luiza');
+alunos.push('Eduardo');
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+// console.log(alunos.slice(0, -2));
+
+// delete alunos[x]; // Deleta o valor de X posição
+
+// const removido = alunos.pop(); // Remove a última posição da array
+
+// alunos.unshift('Marcos'); // Adiciona no começo da array
+
+// alunos.push('Marcos'); // Adiciona no fim da array
+
+// alunos[alunos.lenth] = 'Marcos'; // Adiciona no fim da array
+
+// alunos[0] = 'Marcos'; // Altera o valor da posição 0 da array
+
+// alunos[x] = 'Marcos'; // Adiciona em X posição
