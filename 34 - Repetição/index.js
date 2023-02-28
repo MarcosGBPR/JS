f01e4400aabfa80bb96ef4da80db21ad3bd87ce3
Marcos Gabriel <55326111+MarcosGBPR@users.noreply.github.com>
2023-02-28 17:10:57 -0300

    a

---
+++ 34 - Repetição/index.js
@@ -0,0 +1,18 @@
+// console.log('Linha 5');
+// console.log('Linha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+
+
+// i = index
+// += / -= - Quantidade de 'pulos'
+//for (let i = 0; i <= 10; i ++) {
+//    const par = i % 2 === 0 ? 'par' : 'ímpar';
+//    console.log(i, par);
+//}
+
+const frutas = ['Maçã', 'Pêra', 'Uva', 'Tomate', 'Abacate', 'Melão', 'Laranja', 'Melância'];
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`Índice ${i}`,frutas[i]);
+}
