0fc9f91180626f815c64b207f33d68112c75be17
Marcos Gabriel <marcosgb108@hotmail.com>
2023-03-03 00:41:58 -0300

    a

--- 39 - Exercício/Modelo/assets/js/main.js
+++ 39 - Exercício/Modelo/assets/js/main.js
@@ -1,2 +1,11 @@
 const paragrafos = document.querySelector('.paragrafos');
 const ps = paragrafos.querySelectorAll('p');
+
+const estilosBody = getComputedStyle(document.body);
+const bgColorBody = estilosBody.backgroundColor;
+console.log(bgColorBody);
+
+for (let p of ps) {
+    p.style.backgroundColor = bgColorBody;
+    p.style.backgroundColor = '#FFFFFF';
+};
