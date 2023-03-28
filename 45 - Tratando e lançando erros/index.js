5ec6ade3f9278be79933923e3af6ef39c8ba503c
Marcos Gabriel <marcosgb108@hotmail.com>
2023-03-28 09:16:31 -0300

    a

--- 45 - Tratando e lançando erros/index.js
+++ 45 - Tratando e lançando erros/index.js
@@ -7,7 +7,7 @@
 
 function soma(x, y) {
     if (typeof x !== 'number' || typeof y !== 'number') {
-        throw('x e y precisam ser números.');
+        throw new Error('x e y precisam ser números.');
     }
 
     return x + y;
